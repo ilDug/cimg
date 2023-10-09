f7f6c056c7872d5b904918e5caa5788b2d961e53
ilDug <dognini.marco@gmail.com>
2023-10-09 15:45:19 +0200

    creato i command e funzione di conversione da HEIC

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 import typer
 import sys
+
 from modules.compress import compress_png
 from modules.stats import compression_stats
+
 from pathlib import Path
-from typing import Annotated, Optional
+from typing import Annotated
+from pillow_heif import register_heif_opener
+from PIL import Image
+from rich import print
+
+app = typer.Typer()
 
 
-def main(
+# ////////////////////////////////////////////////////////////////////////////
+@app.command()
+def compress(
     image: Annotated[
-        Optional[str],
+        str,
         typer.Argument(help="il percorso all'immagine da comprimere"),
-    ] = None,
+    ],
     output: Annotated[
-        Optional[str], typer.Argument(help="il percorso dove salvare l'immagine")
+        str, typer.Argument(help="il percorso dove salvare l'immagine")
     ] = None,
     size: Annotated[
         int,
@@ -32,7 +41,7 @@ def main(
     ] = 70,
 ):
     """
-    cimg -s 768 -q 70  input_image.png  [output_image.png]
+    cimg compress -s 768 -q 70  input_image.png  [output_image.png] || [ > stdout ]
 
     Comprime un'immagine con le dimensioni e la qualità volute.
 
@@ -55,8 +64,53 @@ def main(
     if output is not None and image is not None:
         dest = Path(output)
         compression_stats(source, dest)
+    else:
+        print("Compressione terminata")
+
+
+# ////////////////////////////////////////////////////////////////////////////
+@app.command()
+def convert(
+    image: Annotated[
+        str,
+        typer.Argument(help="il percorso all'immagine da comprimere"),
+    ],
+    output: Annotated[
+        str, typer.Argument(help="il percorso dove salvare l'immagine")
+    ] = None,
+):
+    """
+    cimg convert input_image.heic  [output_image.png] || [ > stdout ]
+
+    Converte l'immagine da formati tipo HEIC a JPEG.
+
+    NON ACCETTA stdin, ma può essere salvata con lo stdout:
+
+    """
+
+    register_heif_opener()
+    source = Path(image)
+    with Image.open(source) as img:
+        if output is None:
+            # print oto stdout
+            img.save(sys.stdout, optimize=True, format="jpeg")
+            print("Coversione terminata")
+
+        else:
+            # sovrascrive il file sorgente
+            img.save(output, optimize=True, format="jpeg")
+            print(
+                f"Coversione terminata, salvato il file: [bold green]{output}[/bold green]"
+            )
+
+
+# ////////////////////////////////////////////////////////////////////////////
+@app.callback()
+def main():
+    print("cimg version 0.2.0")
 
 
 ###############################################################################
 if __name__ == "__main__":
-    typer.run(main)
+    # typer.run(main)
+    app()
